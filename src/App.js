49c8b306e15eab32e487f744332f4357606135d3
Divyanshi Rastogi <43536638+Divya-5@users.noreply.github.com>
2021-09-30 22:35:17 +0530

    added rating

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ const playlist = {
     {
       name: "Family Ties",
       link:
-        "https://open.spotify.com/track/7Bpx2vsWfQFBACRz4h3IqH?si=8f89625a89be4d4d"
+        "https://open.spotify.com/track/7Bpx2vsWfQFBACRz4h3IqH?si=8f89625a89be4d4d",
+      rating: "3/5"
     },
     {
       name: "Lonely",
       link:
-        "https://open.spotify.com/track/6pmNoWKk0r6zfIjWneJRxR?si=4efd3ea2c27746de"
+        "https://open.spotify.com/track/6pmNoWKk0r6zfIjWneJRxR?si=4efd3ea2c27746de",
+      rating: "4/5"
     },
     {
       name: "Red Light",
       link:
-        "https://open.spotify.com/track/1B6CUUecnY04JvciEjSVfh?si=797b230bb7cc4278"
+        "https://open.spotify.com/track/1B6CUUecnY04JvciEjSVfh?si=797b230bb7cc4278",
+      rating: "5/5"
     }
   ],
 
@@ -23,34 +26,40 @@ const playlist = {
     {
       name: "Stormy Weather",
       link:
-        "https://open.spotify.com/track/6ffJxrVKkPVW4hiRoiteB2?si=ed38b9c0240848f7"
+        "https://open.spotify.com/track/6ffJxrVKkPVW4hiRoiteB2?si=ed38b9c0240848f7",
+      rating: "2/5"
     },
     {
       name: "Search For Peace",
       link:
-        "https://open.spotify.com/track/4qRJ7ZJyfzchEd2fjle7Bz?si=41ed72f04e8c4d17"
+        "https://open.spotify.com/track/4qRJ7ZJyfzchEd2fjle7Bz?si=41ed72f04e8c4d17",
+      rating: "3/5"
     },
     {
       name: "Blue World",
       link:
-        "https://open.spotify.com/track/5z8eOnPiOC22EYpniAX3Qg?si=e60536b1f75a4146"
+        "https://open.spotify.com/track/5z8eOnPiOC22EYpniAX3Qg?si=e60536b1f75a4146",
+      rating: "4/5"
     }
   ],
   Oldies: [
     {
       name: "Dance with Me",
       link:
-        "https://open.spotify.com/track/2GORop0i2cyx8C0A3JzL44?si=7d019f704ebc46f8"
+        "https://open.spotify.com/track/2GORop0i2cyx8C0A3JzL44?si=7d019f704ebc46f8",
+      rating: "3/5"
     },
     {
       name: "Still the one",
       link:
-        "https://open.spotify.com/track/2dtK02TSAuTvVYU2wGAVG0?si=c0a19de7e5564c71"
+        "https://open.spotify.com/track/2dtK02TSAuTvVYU2wGAVG0?si=c0a19de7e5564c71",
+      rating: "5/5"
     },
     {
       name: "You send me ",
       link:
-        "https://open.spotify.com/track/0BFEyqJ9DJXS7gKg0Kj46R?si=0ed042ae711149fe"
+        "https://open.spotify.com/track/0BFEyqJ9DJXS7gKg0Kj46R?si=0ed042ae711149fe",
+      rating: "4/5"
     }
   ]
 };
@@ -80,6 +89,7 @@ export default function App() {
                 <a href={song.link} target="_blank" rel="noreferrer">
                   Listen Song
                 </a>
+                <p>Rating:{song.rating}</p>
               </div>
             </li>
           ))}
